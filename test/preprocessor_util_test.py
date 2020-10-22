ccafdace8d2bad0497c61838d0bc3cd7f70a4fb9
Gavin <gaprice@lbl.gov>
2020-10-21 19:44:41 -0700

    typo

--- test/preprocessor_util_test.py
+++ test/preprocessor_util_test.py
@@ -7,7 +7,7 @@ from StaticNarrative.exporter.processor_util import build_report_view_data
 def test_Report_with_None_direct_link_index_and_truthy_html_links():
     '''
     Tests the case where a report contains a direct_html_link_index key with a value of None
-    and a truthy html_links entry. This cause a None vs. int comparison error in an earlier
+    and a truthy html_links entry. This caused a None vs. int comparison error in an earlier
     version of the code. See PUBLIC-1411
     '''
     ws = create_autospec(Workspace, spec_set=True, instance=True)
