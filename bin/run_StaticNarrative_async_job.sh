beb195f9c5fd82a23f9864eb63212113ca2084c2
ialarmedalien <ialarmedalien@gmail.com>
2023-03-09 15:23:47 -0800

    quoting shell variables

--- bin/run_StaticNarrative_async_job.sh
+++ bin/run_StaticNarrative_async_job.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 script_dir=$(dirname "$(readlink -f "$0")")
-export PYTHONPATH=$script_dir/../lib:$PATH:$PYTHONPATH
+export PYTHONPATH="$script_dir"/../lib:"$PATH":"$PYTHONPATH"
 python -u "$script_dir"/../lib/StaticNarrative/StaticNarrativeServer.py $1 $2 $3
